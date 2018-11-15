09e9e54da9984e0b241f59164ba4574727ea1b48
Korneel Eeckhout <korneel@pienter.tech>
2018-11-16 00:01:34 +0100

    structure

---
+++ ex05_super-slider/super-slider.js
@@ -0,0 +1,117 @@
+const newSlider = document.querySelector('.super-slider');
+
+/**
+ * @param ssSlide {Element}
+ * @param bullets {Element}
+ * @param width {number}
+ * @param index {number}
+ */
+function slide(ssSlide, bullets, width, index) {
+  // make the image slide:
+  // change index of ssSlide
+  // change left of ssSlide
+  // set active class on correct .ss-bullet
+}
+
+/**
+ * @param imgElement {Element}
+ * @param newWidth {number}
+ * @returns {Element}
+ */
+function resizeImg(imgElement, newWidth) {
+  // set imgElement.syle.width and -height
+}
+
+/**
+ * @param element {Element}
+ * @param containerWidth {number}
+ * @returns {NodeListOf<Element>}
+ */
+function resizeImages(element, containerWidth) {
+  // resizeImg for all images
+  // return images
+}
+
+/**
+ * @param element {Element}
+ * @param images {NodeListOf<Element>}
+ * @returns {Element}
+ */
+function makeSsSlide(element, images) {
+  // create ssSlide (see html document for correct structure)
+  // add classes and index
+  // append all images
+  // return ssSlide
+}
+
+/**
+ * @param leftRight {string}
+ * @returns {Element}
+ */
+function makeArrow(leftRight) {
+  // make new arrow (left or right)
+  // add classes and font-awesome icon
+  // see html
+  // return the arrow
+}
+
+/**
+ * @param count {number}
+ * @returns {Element}
+ */
+function makeBullets(count) {
+  // make .ss-bullets
+  // fill with count * .ss-bullet
+  // see html
+  // return bullets
+}
+
+/**
+ * @param element {Element}
+ */
+function init(element) {
+  // remove loading class from element
+  // resize images
+  // append ssSlide, left and right arrow and bullets
+  // add event listeners
+}
+
+/**
+ * @param element {Element}
+ */
+function preLoad(element) {
+  // add loading icon when images are still loading
+  // no need to edit
+  element.classList.add('ss-container', 'loading');
+}
+
+/**
+ * @param element {Element}
+ */
+function superSlider(element) {
+  // checks if all images are loaded then initiates superSlider
+  // no need to edit
+  preLoad(element);
+  const images = element.querySelectorAll('img');
+  let imagesLoaded = 0;
+  images.forEach(function (img) {
+    if (img.complete) {
+      imagesLoaded++;
+      if (imagesLoaded === images.length) {
+        init(element);
+      }
+    } else {
+      img.addEventListener('load', function () {
+        imagesLoaded++;
+        if (imagesLoaded === images.length) {
+          init(element);
+        }
+      });
+    }
+  });
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  superSlider(newSlider);
+});
